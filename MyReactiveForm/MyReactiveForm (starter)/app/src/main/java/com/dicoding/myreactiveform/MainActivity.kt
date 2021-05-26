a6c455d057dd1bfecafbda97ccb499f62dcd8ce9
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-27 00:22:02 +0700

    add data stream from input email, then subscribe to the stream

--- MyReactiveForm/MyReactiveForm (starter)/app/src/main/java/com/dicoding/myreactiveform/MainActivity.kt
+++ MyReactiveForm/MyReactiveForm (starter)/app/src/main/java/com/dicoding/myreactiveform/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dicoding.myreactiveform
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,6 +8,7 @@ import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.dicoding.myreactiveform.databinding.ActivityMainBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
 
 /**
 Terdapat beberapa masalah yang dapat diatasi dengan Reactive Programming, yaitu seperti :
@@ -23,10 +25,31 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //data stream dari inputan Email dilanjutkan dengan subscribe ke stream tersebut
+        val emailStream = RxTextView.textChanges(binding.edEmail)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe {
+            showEmailExistAlert(it)
+        }
+
+        //hal yang sama untuk inputan password
+        val passwordStream = RxTextView.textChanges(binding.edPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe {
+            showPasswordMinimalAlert(it)
+        }
 
     }
 
