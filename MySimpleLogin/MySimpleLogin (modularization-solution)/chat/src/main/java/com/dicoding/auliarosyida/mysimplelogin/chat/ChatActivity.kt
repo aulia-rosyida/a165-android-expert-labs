3e379ffd98f4936114b873cff480cdd037d71be1
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-13 23:09:30 +0700

    show data from SessionManager

--- MySimpleLogin/MySimpleLogin (modularization-solution)/chat/src/main/java/com/dicoding/auliarosyida/mysimplelogin/chat/ChatActivity.kt
+++ MySimpleLogin/MySimpleLogin (modularization-solution)/chat/src/main/java/com/dicoding/auliarosyida/mysimplelogin/chat/ChatActivity.kt
@@ -2,10 +2,19 @@ package com.dicoding.auliarosyida.mysimplelogin.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.dicoding.core.SessionManager
+import com.dicoding.core.UserRepository
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        //untuk menampilkan data dari SessionManager.
+        val tvChat = findViewById<TextView>(R.id.tv_chat)
+        val sesi = SessionManager(this)
+        val userRepository = UserRepository.getInstance(sesi)
+        tvChat.text = "Hello ${userRepository.getUser()}! \n Welcome to Chat Feature"
     }
 }
