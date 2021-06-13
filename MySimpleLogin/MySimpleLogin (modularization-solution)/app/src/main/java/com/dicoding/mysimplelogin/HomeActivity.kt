e0bfb3cd6c3f96c12054b31b1e71bb9e721f3de9
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-13 23:13:12 +0700

    navigation to module chat when fab clicked

--- MySimpleLogin/MySimpleLogin (modularization-solution)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ MySimpleLogin/MySimpleLogin (modularization-solution)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.dicoding.core.SessionManager
 import com.dicoding.core.UserRepository
 import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
@@ -26,6 +27,19 @@ class HomeActivity : AppCompatActivity() {
             userRepository.logoutUser()
             moveToMainActivity()
         }
+
+        //kode untuk Navigasi ke module Chat ketika FloatingActionButton diklik pada HomeActivity
+        binding.fab.setOnClickListener {
+            try {
+                moveToChatActivity()
+            } catch (e: Exception){
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.dicoding.mysimplelogin.chat.ChatActivity")))
     }
 
     private fun moveToMainActivity() {
